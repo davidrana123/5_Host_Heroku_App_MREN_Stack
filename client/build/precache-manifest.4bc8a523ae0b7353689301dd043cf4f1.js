f49ebdc1531ddf9599387f49be4580fd1454558f
David <david123rana@gmail.com>
2021-09-09 05:34:54 -0700

    static file

---
+++ client/build/precache-manifest.4bc8a523ae0b7353689301dd043cf4f1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c538e2f4473c448a5009ad57d49621d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c05c1005d93cf5178bee",
+    "url": "/static/css/main.4b169742.chunk.css"
+  },
+  {
+    "revision": "e40cc7a30c7b862498ff",
+    "url": "/static/js/2.7eea6573.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "/static/js/2.7eea6573.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c05c1005d93cf5178bee",
+    "url": "/static/js/main.b1934496.chunk.js"
+  },
+  {
+    "revision": "5f376c656942d8cc8b0a",
+    "url": "/static/js/runtime-main.99dbf700.js"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "f0ed69e5a01dc22401c679096aadc43f",
+    "url": "/static/media/SanjayDhar.f0ed69e5.jpg"
+  },
+  {
+    "revision": "3cb2bd8af52642dc0ddbdbba41a611ca",
+    "url": "/static/media/aboutus.3cb2bd8a.jpg"
+  },
+  {
+    "revision": "4097fb6016cf84838a19b4f72df27db7",
+    "url": "/static/media/abt-us.4097fb60.jpg"
+  },
+  {
+    "revision": "66a731dd8ef0a7381621fa522db67be4",
+    "url": "/static/media/abtus.66a731dd.jpg"
+  },
+  {
+    "revision": "0ae74b4dd41616462335e532b3ff0887",
+    "url": "/static/media/activity.0ae74b4d.svg"
+  },
+  {
+    "revision": "ce33f55e71dd6b6c2630fa535d19e27c",
+    "url": "/static/media/announces.ce33f55e.jpg"
+  },
+  {
+    "revision": "20ffefcfbb522e9a16af7b8c3b0373c9",
+    "url": "/static/media/dlittphD.20ffefcf.jpeg"
+  },
+  {
+    "revision": "14b9d835e4129ba712ce5c2dc015b790",
+    "url": "/static/media/identify.14b9d835.svg"
+  },
+  {
+    "revision": "6879155bf55db2635830499340c71254",
+    "url": "/static/media/nirmohiUncle.6879155b.jpg"
+  },
+  {
+    "revision": "da6c70498ea73517852fc2ddb58d100e",
+    "url": "/static/media/prakharLogo.da6c7049.jpeg"
+  },
+  {
+    "revision": "520ebd6482397364760d303b247881cd",
+    "url": "/static/media/talent.520ebd64.svg"
+  },
+  {
+    "revision": "984834238e7ce429c957b55d9c0a9166",
+    "url": "/static/media/vision.98483423.jpg"
+  },
+  {
+    "revision": "e4eb9785cef3aaea78b4899fcadfc4ab",
+    "url": "/static/media/wallpaper2you_80589.e4eb9785.jpg"
+  }
+]);
